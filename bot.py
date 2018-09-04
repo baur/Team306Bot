2ea07d752b522f90cac0c54808be621cf1da6cff
Baur <baurdontet@gmail.com>
2018-09-05 01:50:18 +0600

    commit

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(commands=['start', 'go'])
 def start_handler(message):
-    bot.send_message(message.chat.id, 'Привет, когда я вырасту, я буду парсить заголовки с Хабра')
+    bot.send_message(message.chat.id, 'команда start')
 
 @bot.message_handler(content_types=["text"])
 #def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
@@ -15,9 +15,11 @@ def text_handler(message):
     text = message.text.lower()
     chat_id = message.chat.id
     if text == "привет":
-        bot.send_message(chat_id, 'Привет, я бот - парсер хабра.')
+        bot.send_message(chat_id, 'Привет, я бот - АСУТП')
     elif text == "как дела?":
         bot.send_message(chat_id, 'Хорошо, а у тебя?')
+    elif text == "бауыржан":
+        bot.send_message(chat_id, '87051110181')		
     else:
         bot.send_message(chat_id, 'Простите, я вас не понял :(')
 
